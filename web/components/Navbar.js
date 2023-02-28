0878c7fbd9e4e1bf67c26744a01e6792bfcafd67
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2023-02-28 16:26:25 +0530

    Update Navbar.js

--- web/components/Navbar.js
+++ web/components/Navbar.js
@@ -10,12 +10,13 @@ export default function Navbar() {
 
   return (
     <>
-     <nav className=' flex justify-between h-12 text-white bg-black   font-bold' >
-        <span className='mx-6 my-2 flex text-blue-400 text-3xl	'><Link href={"/"}>Raise</Link></span>
+     <nav className=' flex justify-between h-12 text-green-400 bg-black   font-bold' >
+        <span className='mx-8 my-2 flex text-green-400 text-3xl	'><Link href={"/"}>Raise</Link></span>
         <ul className= 'px-2 py-3 flex space-x-10 mx-6 '>
     {/* <div className='flex mx-16 '> */}
       {/* <input className="px-4 h-8  rounded-lg  bg-gray-800 text-gray-400" type="text" placeholder="🔍 Search projects" /> */}
 {/* </div> */}
+
 <div className='ease-in-out delay-50 hover:-translate-y-1 text-lg font-semibold '>
       <Link href="/">Home</Link></div>
 <div className='ease-in-out delay-50 hover:-translate-y-1 text-lg font-semibold '>
@@ -28,7 +29,7 @@ export default function Navbar() {
 
       <Link href="/fund">Fund</Link></div>
       {/* <div className=' mx-2 my-2 '> */}
-<div className='ease-in-out delay-50 hover:-translate-y-1 text-lg font-semibold bg-white text-black rounded-lg px-2'>
+<div className='ease-in-out delay-50 hover:-translate-y-1 text-lg font-semibold bg-green-400 text-black rounded-lg px-3'>
       <button onClick={currentUser.addr ? logOut : logIn}>
         {currentUser.addr ? `${currentUser.addr}` : "Login"}
       </button></div>
